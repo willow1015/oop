7f0ca764bc99603772aedff3f6afe77c9faf6390
Your Name <you@example.com>
2020-07-10 15:17:20 +0900

    네모 클릭

--- src/main/java/oop/App.java
+++ src/main/java/oop/App.java
@@ -9,9 +9,18 @@ public class App
         Rectangle rect1 = new Rectangle.Builder(new Vector2d(0, 0))
             .setSize(new Vector2d(50, 50))
             .build();
-        Rectangle rect2 = new Rectangle.Builder(new Vector2d(150, 150))
+        final Rectangle rect2 = new Rectangle.Builder(new Vector2d(150, 150))
             .setSize(new Vector2d(50, 50))
             .build();
+
+        rect2.addOnClickListener(new OnClickListener(){
+        
+            @Override
+            public void Onclick(Vector2d position) {
+                rect2.position.add(new Vector2d(10, 10));
+                
+            }
+        });
 
         final Circle circle1 = new Circle(new Vector2d(100, 100), 25);
         circle1.addOnClickListener(new OnClickListener(){
