684c5d6a246778c1b40751f28d6bb395becc1be5
Your Name <you@example.com>
2020-07-10 14:32:24 +0900

    제대로 된 수정

--- src/main/java/oop/Renderer.java
+++ src/main/java/oop/Renderer.java
@@ -5,7 +5,6 @@ import java.awt.Graphics;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.util.ArrayList;
-import java.awt.color.*;
 
 import javax.swing.JFrame;
 
